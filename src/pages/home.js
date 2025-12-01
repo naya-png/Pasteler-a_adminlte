501bd2b74a1281d2d1ecb866944914bd62efb2b2
naya-png <na.leyton@duocuc.cl>
2025-12-01 19:46:00 -0300

    Avances iniciales

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarPrincipal from "./components/navbar";
+
+function Home() {
+    return (
+        <div>
+            <NavbarPrincipal/> 
+        </div>
+    );
+}
+
+export default Home;
